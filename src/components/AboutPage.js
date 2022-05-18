3581ae70afe16e37cab6bf698b1c4e08dba0b6af
Yash <bajajy82@gmail.com>
2022-05-18 15:57:25 +0530

    file created for all pages and created allSvgs file

---
+++ src/components/AboutPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AboutPage() {
+  return <div>AboutPage Component Page</div>;
+}
+
+export default AboutPage;
---
+++ src/subComponents/PowerButton.js
@@ -0,0 +1,27 @@
+// Home Button
+
+import React from "react";
+import styled from "styled-components";
+import { PowerBtn } from "../components/AllSvgs";
+
+const Power = styled.button`
+  position: fixed;
+  top: 2rem;
+  left: 50%;
+  transform: translate(-50%, 0);
+
+  background-color: #FCF6F4;
+  padding: 0.3rem;
+  border-radius: 50%;
+  border: 1px solid #000;
+  width: 2.5rem;
+  height: 2.5rem;
+`;
+
+function PowerButton() {
+  return <Power>
+    <PowerBtn width={30} height={30} fill="currentColor" />
+  </Power>;
+}
+
+export default PowerButton;
